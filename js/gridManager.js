0b0c68450d6be421b143060f2812d1e5cc682f60
Eric <erikbc_95@hotmail.com>
2018-01-02 01:05:38 +0100

    changed caph style for virtual keyboard

--- js/gridManager.js
+++ js/gridManager.js
@@ -4,15 +4,21 @@ $(document).ready(function(){
 	$.caph.focus.activate(function(nearestFocusableFinderProvider, controllerProvider) {
 		
 	    controllerProvider.onFocused(function(event, originalEvent) {
-	    	$(event.currentTarget).css({
-				border: '3px solid red'
-				});
+	    	
 	    	
 	    	//if map is ready
 	    	if($('.countryPanel').length > 0){
 	    		//set to the panel the value of the country
 	    		$('.countryPanel').text($(event.currentTarget).attr('id'));
 	    	}
+	    	//if virtual key is focused set style
+	    	if($(event.currentTarget).attr("class").split(' ')[0] === 'jQKeyboardBtn'){
+	    		$(event.currentTarget).css('background','#26b3f7');
+	    	}else{
+	    		$(event.currentTarget).css({
+					border: '3px solid red'
+				});	
+	    	}  
 	    	
 	    });
 	    
@@ -38,9 +44,15 @@ $(document).ready(function(){
 		});
 	    
 		controllerProvider.onBlurred(function(event, originalEvent) {
-        	$(event.currentTarget).css({
-				border : '3px solid transparent'
-        	});
+			//if virtual key is focused set style
+	    	if($(event.currentTarget).attr("class").split(' ')[0] === 'jQKeyboardBtn'){
+	    		$(event.currentTarget).css('background','#424242');
+	    	}else{
+	    		$(event.currentTarget).css({
+					border : '0px solid transparent'
+	        	});
+	    	}
+        	
 	    });
 	
 	});	
